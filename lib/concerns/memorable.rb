297729409aea1fcdfe9bb603a1b07b23d0f46123
Mike Cooper <mikescooper@icloud.com>
2019-09-20 02:09:52 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,9 +1,11 @@
 module Memorable 
-   def reset_all
-    self.all.clear
-  end
-
-  def count
-    self.all.count
+  module ClassMethods
+     def reset_all
+      self.all.clear
+    end
+  
+    def count
+      self.all.count
+    end
   end
 end 
